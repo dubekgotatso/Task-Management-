35f00fe1bd8543c36b669a2556476fbf1bf02828
kgotatsosurprise303@gmail.com <kgotatsosurprise303@gmail.com>
2024-08-06 10:06:49 +0200

    fixing user controllers and user routes for signup so that i can get a result when testing, but still i'm getting an error

--- App/Controllers/user_controllers.py
+++ App/Controllers/user_controllers.py
@@ -7,8 +7,10 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 from flask_bcrypt import Bcrypt
 
 
+# The signup function handles the user registration process
 
 def signup_user():
+ if request.method == "POST":
     username = request.json.get('username')
     email = request.json.get('email')
     password = request.json.get('password')
@@ -19,34 +21,33 @@ def signup_user():
     # Check if user already exists
     existing_user = User.find_user_by_username(username)
     if existing_user:
-        signupdetails = {'username': username, 'email': email, 'password': password}
         return jsonify({'message': 'User already exists'}), 400
 
     # Create new user
     hashed_password = generate_password_hash(password, method='sha256')
-    new_user = User.create_user(username, email, hashed_password)
+    new_user = User.create_user(username, email, hashed_password,)
     
     if not new_user:
         return jsonify({'message': 'Failed to create user'}), 500
 
-    access_token = create_access_token(identity=new_user.id)
+    access_token = create_access_token(identity=str(new_user['_id']))
     
     return jsonify({'message': 'User created successfully', 'access_token': access_token}), 201
 
+# def login_user():
+#     username = request.json.get('username')
+#     password = request.json.get('password')
 
-def loginUser():
-    username = request.json.get('username')
-    password = request.json.get('password')
+#     if not username or not password:
+#         return jsonify({'message': 'Username and password are required'}), 400
 
-    if not username or not password:
-        return jsonify({'message': 'Username and password are required'}), 400
+#     # Retrieve user from database (adjust based on your implementation)
+#     user = User.find_user_by_username(username=username).first()
 
-    # Retrieve user from database (adjust based on your implementation)
-    user = User.find_user_by_username(username=username).first()
+#     if not user or not User.check_password_hash(user.password, password):
+#         return jsonify({'message': 'Invalid username or password'}), 401
+#     else:
+#         return jsonify({'message': 'Successfully logged in'})
 
-    if not user or not check_password_hash(user.password, password):
-        return jsonify({'message': 'Invalid username or password'}), 401
 
-    access_token = create_access_token(identity=user.id)
-    
-    return jsonify({'message': 'Login successful', 'access_token': access_token}), 200
+
